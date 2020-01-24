4e0d6ea0928e90a8145b8ed4e9b323f0d7674a46
Nicholas Anna <nanna@engineeredhere.com>
2020-01-24 13:39:02 -0500

    setting up redux store

--- src/reduxElements/actions.js
+++ src/reduxElements/actions.js
@@ -3,3 +3,18 @@ Functions that return actions.
 These actions can be `dispatched` by mapping them
 to a property of a react component.
 **/
+
+export const placeList = (newList) => ({
+  type: 'PLACE_LIST',
+  data: newList,
+});
+
+export const addItem = (newItem) => ({
+  type: 'ADD_ITEM',
+  data: newItem,
+});
+
+export const deleteIndex = (index) => ({
+  type: 'DELETE_INDEX',
+  data: index,
+});
--- src/reduxElements/index.js
+++ src/reduxElements/index.js
@@ -5,11 +5,37 @@ a new, updated state to the redux store
 **/
 
 // Initial State
-const initState = {};
+const initState = {
+  list: [],
+};
 
 // This is the
 export default function(state = initState, action) {
   switch(action.type) {
+    case 'PLACE_LIST':
+      return {
+        ...state,
+        list: action.data,
+      };
+
+    case 'ADD_ITEM':
+      return {
+        ...state,
+        list: [
+          ...state.list,
+          action.data,
+        ],
+      };
+
+    case 'DELETE_INDEX':
+      return {
+        ...state,
+        list: [
+          ...state.list.slice(0, action.data),
+          ...state.list.slice(action.data + 1)
+        ],
+      };
+
     default:
       return state;
   }
